1e2cff0800a42cb6bfa3961b678e24765c1dedba
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 10:46:33 +0900

    とりあえずデフォルトレイアウトの作成

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="default-layout">
+    <div class="default-layout__header">Header</div>
+    <div class="default-layout__container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.default-layout {
+  &__header {
+  }
+
+  &__container {
+    max-width: 64rem;
+    margin: 0 auto;
+  }
+}
+</style>
