92eec4f49d6635e201dcc900a567b8bcd0f5a703
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 13:40:52 +0900

    TaskList, Taskの枠組み作成

---
+++ components/molecules/Task.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="task">
+    <div class="task__content">
+      <Checkbox :label="content" />
+    </div>
+    <div class="task__actions">
+      <IconButton icon="edit" />
+      <IconButton icon="delete" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Task extends Vue {
+  @Prop({ type: String })
+  readonly content!: string
+}
+</script>
---
+++ components/molecules/TaskList.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="task-list">
+    <li v-for="i in 10" :key="i">
+      <Task :content="`Task ${i}`" />
+    </li>
+  </ul>
+</template>
