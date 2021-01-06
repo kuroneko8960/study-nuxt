313b64f12c98ea065a38953d9fc17b35285ff694
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 14:43:18 +0900

    index pageのスタイル適用

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="index-page">
-    <h1>タスク一覧</h1>
-    <TaskListView />
+    <div class="index-page__header">
+      <h1>タスク一覧</h1>
+    </div>
+    <div class="index-page__body">
+      <TaskListView />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.index-page {
+  margin: 1rem 0;
+
+  &__header {
+    h1 {
+      font-size: 1.25em;
+      line-height: 1.5;
+      border-bottom: 1px solid #009688;
+    }
+
+    margin-bottom: 1rem;
+  }
+}
+</style>
