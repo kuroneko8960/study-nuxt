79ceb973f81e7e4834e9efce86795b9e85892adf
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 13:19:55 +0900

    IconButtonコンポーネントの枠組み作成

---
+++ components/atoms/IconButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="icon-button" type="button">
+    <i class="material-icons">{{ icon }}</i>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class C extends Vue {
+  @Prop({ type: String })
+  readonly icon!: string
+}
+</script>
