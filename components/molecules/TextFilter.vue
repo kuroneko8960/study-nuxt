b2d9c0544e8af678859bc19904138f05b22b8a7b
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 14:35:09 +0900

    TextFilterのプロパティ定義を追加

--- components/molecules/TextFilter.vue
+++ components/molecules/TextFilter.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="text-filter">
     <i class="material-icons">filter_alt</i>
-    <TextField :placeholder="placeholder" />
+    <TextField v-model="value" :placeholder="placeholder" />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class TextFilter extends Vue {
+  @Prop({ type: String })
+  readonly value!: string
+
+  @Prop({ type: String })
+  readonly placeholder!: string
+}
+</script>
--- components/organisms/TaskListView.vue
+++ components/organisms/TaskListView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="task-list-view">
     <div class="task-list-view__filter">
-      <TextFilter />
+      <TextFilter placeholder="表示するタスクをフィルター" />
     </div>
     <div class="task-list-view__list">
       <TaskList />
