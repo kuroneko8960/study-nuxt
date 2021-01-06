91ddaf369fb2f196d43eaf1da2f1904a99b29f6e
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 14:34:18 +0900

    TextFieldコンポーネントの外枠作成

---
+++ components/atoms/TextField.vue
@@ -0,0 +1,28 @@
+<template>
+  <label class="text-field">
+    <span v-show="label" class="text-field__label">{{ label }}</span>
+    <input
+      v-model="value"
+      type="text"
+      class="text-field__input"
+      :placeholder="placeholder"
+    />
+  </label>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class TextField extends Vue {
+  @Prop({ type: String })
+  readonly value!: string
+
+  @Prop({ type: String })
+  readonly label!: string
+
+  @Prop({ type: String })
+  readonly placeholder!: string
+}
+</script>
