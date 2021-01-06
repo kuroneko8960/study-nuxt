c4d39d7a5e27bb82233dee4cf51b4cc3f0daccb9
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 16:22:04 +0900

    TaskListのスタイル適用

--- components/molecules/TaskList.vue
+++ components/molecules/TaskList.vue
@@ -1,7 +1,17 @@
 <template>
   <ul class="task-list">
-    <li v-for="i in 10" :key="i">
+    <li v-for="i in 10" :key="i" class="task-list__item">
       <Task :content="`Task ${i}`" />
     </li>
   </ul>
 </template>
+
+<style lang="scss" scoped>
+.task-list {
+  margin: -1em 0;
+
+  &__item {
+    margin: 1em 0;
+  }
+}
+</style>
