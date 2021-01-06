6e46f09f721b7c3dfb6300d606743383cd23c7f5
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 14:47:40 +0900

    TextFilterのスタイルを適用

--- components/molecules/TextFilter.vue
+++ components/molecules/TextFilter.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="text-filter">
-    <i class="material-icons">filter_alt</i>
-    <TextField v-model="value" :placeholder="placeholder" />
+    <div class="text-filter__icon">
+      <i class="material-icons">filter_alt</i>
+    </div>
+    <div class="text-filter__input">
+      <TextField v-model="value" :placeholder="placeholder" full-width />
+    </div>
   </div>
 </template>
 
@@ -18,3 +22,22 @@ export default class TextFilter extends Vue {
   readonly placeholder!: string
 }
 </script>
+
+<style lang="scss" scoped>
+.text-filter {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  &__icon {
+    flex-shrink: 0;
+    flex-grow: 0;
+    color: #616161;
+  }
+
+  &__input {
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+}
+</style>
