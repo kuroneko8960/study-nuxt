b6d5582f00821c971c3773c5a2d4e3d7438674d8
kuroneko8960 <kuroneko46@rire.sakura.ne.jp>
2021-01-06 11:46:41 +0900

    TaskTableの枠組み作成

---
+++ components/molecules/TaskTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <table class="task-table">
+    <thead class="task-table__head">
+      <tr>
+        <th>タスク名</th>
+        <th>操作</th>
+      </tr>
+    </thead>
+    <tbody class="task-table__body">
+      <tr v-for="idx in 5" :key="idx">
+        <td>
+          <Checkbox />
+          Task {{ idx }}
+        </td>
+        <td>
+          <IconButton icon="edit" />
+          <IconButton icon="delete" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
